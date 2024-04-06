17fc5d496b8ed495476546cc936b1cc2c9922684
alexafon <158675700+alexafon@users.noreply.github.com>
2024-04-05 21:18:28 -0400

    Update alexafon_HW4_Q1.cpp

--- alexafon_HW4_Q1.cpp
+++ alexafon_HW4_Q1.cpp
@@ -1,9 +1,48 @@
-using namespace std;
+#include <iostream>
 #include <string>
 #include <vector>
-#include <iostream>
+#include "SocialMediaAccount.h"
+#include "Twitter.h"
+#include "Instagram.h"
+#include <ctime>
+#include <cstdlib>
+#include <random>
 
-int main()
-{
-    
+struct Profile {
+	int userId;
+	string username;
+};
+
+int main() {
+	srand(time(0));
+
+	Twitter<string> TS("TwitterS");
+	Twitter<Profile> TP(Profile{ 1, "TwitterP" });
+
+	Instagram<string> IS("InstaS");
+	Instagram<Profile> IP(Profile{ 2, "InstaP" });
+
+	TS.addFollower("Anonymous User");
+	TS.addFollowed("Anonymous User");
+	TS.displayFollowers();
+	TS.displayFollowed();
+
+	TP.addFollower(Profile{ 3, "Anonymous User" });
+	TP.addFollowed(Profile{ 4, "Anonymous User" });
+	TP.displayFollowers();
+	TP.displayFollowed();
+
+	IS.addFollower("Anonymous User");
+	IS.addFollowed("Anonymous User");
+	IS.displayFollowers();
+	IS.displayFollowed();
+
+	IP.addFollower(Profile{ 5, "Anonymous User" });
+	IP.addFollowed(Profile{ 6, "Anonymous User" });
+	IP.displayFollowers();
+	IP.displayFollowed();
+
+	return 0;
+
 }
+
